46ca0f4e9961141d0fd78b451007857fdf29709e
lloydandersen <lloydandersenco@gmail.com>
2024-06-27 14:45:48 -0700

    Test V.Alpha.1.2

--- main/main.py
+++ main/main.py
@@ -8,6 +8,10 @@ buttons = 0
 latt = 0
 
 
+
+
+
+
 def open_add_file(inp, file_name):
     with open(f"{file_name}.py", "a") as file:
         file.write(f"{inp}")
@@ -16,50 +20,50 @@ def open_add_file(inp, file_name):
 def finish_script():
     open_add_file("""# Start run loop.
 root.mainloop()
-    """, "sample1")
+    """, f"{app_name_var}")
     exit()
 
 
 def start_script():
-    open_add_file("""
-from tkinter import *
+    global app_name_var
+    x = app_name_var.get()
+    app_name_var = x
+    open_add_file(f"""from tkinter import *
 from tkinter import ttk
 root = Tk()
 root.geometry('300x300')
-root.title("Sample one")
-    """, "sample1")
+root.title("{app_name_var}")
+    """, f"{app_name_var}")
 
 
 def add_page():
     global frames
     frames += 1
-    print(frames)
     open_add_file(f"""
 frame{frames} = Frame(root)
 frame{frames}.grid(row=0, column={frames})
 
-""", "sample1")
+""", f"{app_name_var}")
 
 
 def add_button():
     global buttons, frames
     buttons += 1
-    print(buttons)
     open_add_file(f"""
 b_{buttons} = Button(frame{frames}, text="click me")
 b_{buttons}.grid(row={buttons}, column=0)
 
-    """, "sample1")
+    """, f"{app_name_var}")
 
 
 def add_label():
     global latt, frames
     latt += 1
     open_add_file(f"""
-l_{latt} = Label(frame{frames}, text="hey kids")
+l_{latt} = Label(frame{frames}, text="hey what up")
 l_{latt}.grid(row={frames},column=1)
 
-    """, "sample1")
+    """, f"{app_name_var}")
 
 
 root = Tk()
@@ -67,6 +71,11 @@ root.geometry("200x750")
 root.title("Tkapp")
 start_frame = Frame(root)
 start_frame.grid(row=0, column=0)
+app_name_var = StringVar()
+
+
+app_name_entry = Entry(start_frame, textvariable=app_name_var, width=20)
+app_name_entry.grid(row=2, column=1, sticky=W, pady=12, padx=12)
 
 add_start_button = Button(start_frame, text='start', command=start_script, width=10, height=10)
 add_start_button.grid(row=0, column=0, sticky=W, pady=12, padx=12)
