430fc748f60193b222adc4cdbb2d00f4d1ec0090
lloydandersen <lloydandersenco@gmail.com>
2024-06-27 20:30:32 -0700

    -Added addition functions

--- main/script.py
+++ main/script.py
@@ -5,6 +5,10 @@ from tkinter import *
 frames = 0
 buttons = 0
 labels = 0
+check_buttons = 0
+radio_buttons = 0
+entry_boxes = 0
+combo_boxes = 0
 
 
 # Running Functions
@@ -62,10 +66,32 @@ l_{labels}.grid(row={frames},column=1)
     """, f"{app_name_var}")
 
 
+def add_check_button():
+    global check_buttons, frames
+    check_buttons += 1
+
+
+def add_radio_button():
+    global radio_buttons, frames
+    radio_buttons += 1
+
+
+def add_entry_box():
+    global entry_boxes, frames
+    entry_boxes += 1
+
+
+def add_combo_box():
+    global combo_boxes, frames
+    combo_boxes += 1
+
+
 # Create the Tkapp GUI.
 root = Tk()
-root.geometry("200x750")
+root.iconbitmap('Tkapplogomain.ico')
+root.geometry("400x1000")
 root.title("Tkapp")
+root.iconbitmap()
 start_frame = Frame(root)
 start_frame.grid(row=0, column=0)
 app_name_var = StringVar()
@@ -73,7 +99,7 @@ app_name_var = StringVar()
 
 # GUI Widgets.
 app_name_entry = Entry(start_frame, textvariable=app_name_var, width=20)
-app_name_entry.grid(row=2, column=1, sticky=W, pady=12, padx=12)
+app_name_entry.grid(row=0, column=1, sticky=W, pady=12, padx=12)
 
 add_start_button = Button(start_frame, text='start', command=start_script, width=10, height=10)
 add_start_button.grid(row=0, column=0, sticky=W, pady=12, padx=12)
@@ -88,7 +114,8 @@ add_label_button = Button(start_frame, text='add label', command=add_label, widt
 add_label_button.grid(row=3, column=0, sticky=W, pady=12, padx=12)
 
 add_finish_button = Button(start_frame, text='finish', command=finish_script, width=10, height=10)
-add_finish_button.grid(row=0, column=1, sticky=W, pady=12, padx=12)
+add_finish_button.grid(row=0, column=2, sticky=W, pady=12, padx=12)
+
 
 # Start GUI event loop.
 root.mainloop()
