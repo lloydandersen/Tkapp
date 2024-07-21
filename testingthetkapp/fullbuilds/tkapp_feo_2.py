b5c05ae377d5a536629ebf93430647eac6e6102a
lloydandersen <lloydandersenco@gmail.com>
2024-07-20 19:41:15 -0700

    -Added addition functions

--- testingthetkapp/fullbuilds/tkapp_feo_2.py
+++ testingthetkapp/fullbuilds/tkapp_feo_2.py
@@ -6,7 +6,7 @@ from tkinter import colorchooser
 
 root = tk.Tk()
 root.title("Tkapp")
-root.minsize(500, 500)
+root.minsize(700, 500)
 
 
 # var
@@ -19,12 +19,13 @@ body_font_size_var = tk.StringVar()
 primary_color_text_bool = True
 secondary_color_text_bool = True
 
+# Widget stuff
+widget_name = tk.StringVar()
+
 
 # Set varibles
-body_font_size_var.set(16)
+body_font_size_var.set("16")
 body_font_type_var.set("Times New Roman")
-
-
 
 # Program Lists/dictionaries
 system_font_family_list = list()
@@ -38,26 +39,18 @@ def get_system_font_families():
         system_font_family_list.sort()
 
 
-
-
 def chose_primary_color():
-    try:
-        chosen_color = tk.colorchooser.askcolor(title="Primary Color")
-        primary_color_var.set(chosen_color[1])
-        primary_color_button["relief"] = "flat"
-        primary_color_button["background"] = chosen_color[1]
-    except:
-        primary_color_button["background"] = "white"
+    chosen_color = tk.colorchooser.askcolor(title="Primary Color")
+    primary_color_var.set(f"{chosen_color[1]}")
+    primary_color_button["relief"] = "flat"
+    primary_color_button["background"] = chosen_color[1]
 
 
 def chose_secondary_color():
-    try:
-        chosen_color = tk.colorchooser.askcolor(title="Secondary Color")
-        secondary_color_var.set(chosen_color[1])
-        secondary_color_button["relief"] = "flat"
-        secondary_color_button["background"] = chosen_color[1]
-    except:
-        secondary_color_button["background"] = "white"
+    chosen_color = tk.colorchooser.askcolor(title="Secondary Color")
+    secondary_color_var.set(chosen_color[1])
+    secondary_color_button["relief"] = "flat"
+    secondary_color_button["background"] = chosen_color[1]
 
 
 def primary_text_color_changer(*args):
@@ -86,6 +79,75 @@ def update_color_box_font(*args):
     new_size = body_font_size_var.get()
     primary_color_button["font"] = (f"{new_type}", new_size)
     secondary_color_button["font"] = (f"{new_type}", new_size)
+
+
+# Product view tree functions
+
+# Product name count
+database_count = 0
+encrypted_database_count = 0
+account_database_count = 0
+spreadsheet_count = 0
+variables_count = 0
+connections_count = 0
+
+def add_database_to_datatree():
+    global database_count
+    data_tree.insert("", tk.END, text=f"Database_{database_count}")
+    database_count += 1
+
+
+def add_encrypted_database_to_datatree():
+    global encrypted_database_count
+    data_tree.insert("", tk.END, text=f"Encrptyeddb_{encrypted_database_count}")
+    encrypted_database_count += 1
+
+def add_account_database_to_datatree():
+    global account_database_count
+    data_tree.insert("", tk.END, text=f"Accountdb_{account_database_count}")
+    account_database_count += 1
+
+def add_spreadsheet_to_datatree():
+    global spreadsheet_count
+    data_tree.insert("", tk.END, text=f"Spreadsheet_{spreadsheet_count}")
+    spreadsheet_count += 1
+
+def add_varibles_to_datatree():
+    global variables_count
+    data_tree.insert("", tk.END, text=f"Variable_{variables_count}")
+    variables_count += 1
+
+def add_connections_to_datatree():
+    global connections_count
+    data_tree.insert("", tk.END, text=f"Connection_{connections_count}")
+    connections_count += 1
+
+
+# Data tree functions
+def delete_item_on_data_tree(*args):
+    item_selected = data_tree.selection()[0]
+    data_tree.delete(item_selected)
+
+
+def duplicate_item_on_data_tree(*args):
+    item_selected = data_tree.selection()[0]
+    x = data_tree.item(item_selected)
+    data_tree.insert("", tk.END, text=f"{x['text']}", values=x['values'])
+
+
+# Data edit page
+def make_data_edit_frame():
+    # Get widget info
+    item_selected = data_tree.selection()[0]
+    x = data_tree.item(item_selected)
+    widget_name.set(x['text'])
+    print(x)
+    data_edit_name_label = ttk.Label(data_edit_frame, text="Name")
+    data_edit_name_label.grid(row=1, column=0)
+
+    data_edit_name_entry = ttk.Entry(data_edit_frame, textvariable=widget_name)
+    data_edit_name_entry.grid(row=1, column=1)
+
 
 # Running functions
 get_system_font_families()
@@ -217,16 +279,51 @@ data_edit_frame.grid(row=0, column=4, sticky="nwes")
 data_product_menu_title = ttk.Label(data_items_frame, text="Data Products", font=("Times New Roman", 18))
 data_product_menu_title.grid(row=0, column=0, columnspan=2, padx=10, pady=(5, 0))
 
+# Data Products
+product_height = 5
+product_width = 4
+database_product_button = ttk.Button(data_items_frame, text="DB", command=add_database_to_datatree)
+database_product_button.grid(row=1, column=0, ipady=product_height, ipadx=product_width)
+
+encrypteddb_product_button = ttk.Button(data_items_frame, text="EncrpytedDB", command=add_encrypted_database_to_datatree)
+encrypteddb_product_button.grid(row=1, column=1, ipady=product_height, ipadx=product_width)
+
+account_product_button = ttk.Button(data_items_frame, text="Account", command=add_account_database_to_datatree)
+account_product_button.grid(row=2, column=0, ipady=product_height, ipadx=product_width)
+
+spreadsheet_product_button = ttk.Button(data_items_frame, text="Spreadsheet", command=add_spreadsheet_to_datatree)
+spreadsheet_product_button.grid(row=2, column=1, ipady=product_height, ipadx=product_width)
+
+var_product_button = ttk.Button(data_items_frame, text="Variable", command=add_varibles_to_datatree)
+var_product_button.grid(row=3, column=0, ipady=product_height, ipadx=product_width)
+
+connect_product_button = ttk.Button(data_items_frame, text="Connect", command=add_connections_to_datatree)
+connect_product_button.grid(row=3, column=1, ipady=product_height, ipadx=product_width)
+
 
 # data tree frame
 data_tree = ttk.Treeview(data_tree_frame)
-data_tree.grid(row=0, column=0, sticky="nwes")
+data_tree.grid(row=0, column=0, columnspan=3, sticky="nwes")
 data_tree.heading('#0', text="Data Products")
+
+# Data Tree Buttons
+data_tree_delete_button = ttk.Button(data_tree_frame, text="Delete")
+data_tree_delete_button.grid(row=1, column=0, sticky="nwes")
+data_tree_delete_button.bind("<Double-Button-1>", delete_item_on_data_tree)
+
+data_tree_duplicate_button = ttk.Button(data_tree_frame, text="Duplicate")
+data_tree_duplicate_button.grid(row=1, column=1, sticky="nwes")
+data_tree_duplicate_button.bind("<Button-1>", duplicate_item_on_data_tree)
+
+data_tree_edit_button = ttk.Button(data_tree_frame, text="Edit", command=make_data_edit_frame)
+data_tree_edit_button.grid(row=1, column=2, sticky="nwes")
 
 
 # Data edit frame
 data_edit_title = ttk.Label(data_edit_frame, text="Edit", font=("Times New Roman", 18))
-data_edit_title.grid(row=0, column=0, columnspan=1, sticky="ew", padx=10, pady=(5, 0))
+data_edit_title.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10, pady=(5, 0))
+
+
 
 
 
