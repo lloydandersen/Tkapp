1663fc661c2b1d5e7a6e2bfdfeb775a9729b5ee9
lloydandersen <lloydandersenco@gmail.com>
2024-07-30 23:41:47 -0700

    Tkapp is slowly, but steadily coming along. Simple additions. Corrected by comment and added more features.

--- main/main.py
+++ main/main.py
@@ -49,6 +49,13 @@ var_nineteen = tk.StringVar()
 var_twenty = tk.StringVar()
 var_twentyone = tk.StringVar()
 var_twentytwo = tk.StringVar()
+var_type = tk.StringVar()
+
+# Edit Frame Style
+edit_frame_font = ("Times New Roman", 14)
+entry_box_font = ("Times New Roman", 14)
+style.configure("save.TButton", font=("Poor Richards", 16))
+
 
 # Model Functions
 def add_widget_to_tree(widget):
@@ -56,68 +63,69 @@ def add_widget_to_tree(widget):
     global map_count, control_count, icon_count, table_count
     if widget == "label":
         view_tree.insert("", tk.END, text=f"Label_{label_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "label"])
         label_count += 1
     elif widget == "button":
         view_tree.insert("", tk.END, text=f"Button_{button_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "button"])
         button_count += 1
     elif widget == "entry":
         view_tree.insert("", tk.END, text=f"Entry_{entry_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "entry"])
         entry_count += 1
     elif widget == "selection":
         view_tree.insert("", tk.END, text=f"Selection_{selection_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                                 "selection"])
         selection_count += 1
     elif widget == "image":
         view_tree.insert("", tk.END, text=f"Image_{image_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "image"])
         image_count += 1
     elif widget == "graph":
         view_tree.insert("", tk.END, text=f"Graph_{graph_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "graph"])
         graph_count += 1
     elif widget == "gauge":
         view_tree.insert("", tk.END, text=f"Gauge_{gauge_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "gauge"])
         gauge_count += 1
     elif widget == "data":
         view_tree.insert("", tk.END, text=f"Data_{data_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "data"])
         data_count += 1
     elif widget == "map":
         view_tree.insert("", tk.END, text=f"Map_{map_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "map"])
         map_count += 1
     elif widget == "control":
         view_tree.insert("", tk.END, text=f"Control_{control_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "control"])
         control_count += 1
     elif widget == "icon":
         view_tree.insert("", tk.END, text=f"Icon_{icon_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "icon"])
         icon_count += 1
     elif widget == "table":
         view_tree.insert("", tk.END, text=f"Table_{table_count}",
-                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+                         values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, "table"])
         table_count += 1
 
 
 def delete_selection():
-    selected_item = view_tree.selection()
+    selected_item = view_tree.selection()[0]
     view_tree.delete(selected_item)
 
 
 def duplicate_selection():
-    selected_item = view_tree.selection()
+    selected_item = view_tree.selection()[0]
     selected_name = view_tree.item(selected_item)['text']
     selected_values = view_tree.item(selected_item)['values']
-    view_tree.insert("", tk.END, text=f"{selected_name}", values=f"{selected_values}")
+    view_tree.insert("", tk.END, text=selected_name, values=selected_values)
 
 
 def edit_selection():
-    selected_item = view_tree.selection()
+    selected_item = view_tree.selection()[0]
     edit_item = view_tree.item(selected_item)
     item_values = edit_item['values']
     item_name = edit_item['text']
@@ -125,6 +133,7 @@ def edit_selection():
     # Set app vars
     widget_iid.set(selected_item)
     var_one.set(item_name)
+    var_type.set(item_values[21])
 
     var_two.set(item_values[0])
     var_three.set(item_values[1])
@@ -147,24 +156,117 @@ def edit_selection():
     var_twenty.set(item_values[18])
     var_twentyone.set(item_values[19])
     var_twentytwo.set(item_values[20])
-    print(var_two.get())
-
-
+    if var_type.get() == "label":
+        label_edit_frame()
+    elif var_type.get() == "button":
+        button_edit_frame()
+    elif var_type.get() == "entry":
+        entry_edit_frame()
+    elif var_type.get() == "selection":
+        selection_edit_frame()
+    elif var_type.get() == "image":
+        image_edit_frame()
+    elif var_type.get() == "graph":
+        graph_edit_frame()
+    elif var_type.get() == "gauge":
+        gauge_edit_frame()
+    elif var_type.get() == "data":
+        data_edit_frame()
+    elif var_type.get() == "map":
+        map_edit_frame()
+    elif var_type.get() == "control":
+        control_edit_frame()
+    elif var_type.get() == "icon":
+        icon_edit_frame()
+    elif var_type.get() == "table":
+        table_edit_frame()
 
 
 def save_edited_selection():
-    pass
+    iid = widget_iid.get()
+    value_list = [var_two.get(), var_three.get(), var_four.get(), var_five.get(), var_six.get(), var_seven.get(),
+                  var_eight.get(), var_nine.get(), var_ten.get(), var_eleven.get(), var_twelve.get(),
+                  var_thirteen.get(), var_fourteen.get(), var_fifteen.get(), var_sixteen.get(), var_seventeen.get(),
+                  var_eighteen.get(), var_nineteen.get(), var_twenty.get(), var_twentyone.get(), var_twentytwo.get(),
+                  var_type.get()]
+    view_tree.item(iid, text=var_one.get(), values=value_list)
+    print(view_tree.item(iid)['values'])
 
 
 def clear_edit_frame():
     for child in edit_frame.winfo_children():
         child.destroy()
-    edit_title = tk.Label(edit_frame, text="Edit", font=("Roboto", 30), background=background_color, foreground="white")
-    edit_title.grid(row=0, column=0, columnspan=3, sticky="swen", ipadx=60)
+
+
+def build_edit_frame():
+    clear_edit_frame()
+    title = tk.Label(edit_frame, text="Edit", font=("Roboto", 30), background=background_color, foreground="white")
+    title.grid(row=0, column=0, columnspan=3, sticky="swen", ipadx=60)
+    widget_name_label = tk.Label(edit_frame, text="Name", background=background_color, foreground="white")
+    widget_name_label.grid(row=1, column=0, padx=(15, 5), pady=5)
+    widget_name_entry = tk.Entry(edit_frame, textvariable=var_one)
+    widget_name_entry.grid(row=1, column=1, padx=(5, 15), pady=5)
+
+    # Placement
+    pack_placement_type = ttk.Radiobutton(edit_frame, variable=var_three, value='pack', text="Pack")
+    pack_placement_type.grid(row=2, column=0, sticky="swen")
+    grid_placement_type = ttk.Radiobutton(edit_frame, variable=var_three, value='grid', text="Grid")
+    grid_placement_type.grid(row=3, column=0, sticky="swen")
+
+    placement_choice_button = ttk.Button(edit_frame, text="Select")
+    placement_choice_button.grid(row=2, column=1, rowspan=2, sticky="swen")
+
+    save_button = ttk.Button(edit_frame, text="Save", command=save_edited_selection)
+    save_button.grid(row=4, column=0, columnspan=2, pady=10, ipadx=10, ipady=5)
+
 
 # Widget Edit Frames
 def label_edit_frame():
-    pass
+    build_edit_frame()
+
+
+def button_edit_frame():
+    build_edit_frame()
+
+
+def entry_edit_frame():
+    build_edit_frame()
+
+
+def selection_edit_frame():
+    build_edit_frame()
+
+
+def image_edit_frame():
+    build_edit_frame()
+
+
+def graph_edit_frame():
+    build_edit_frame()
+
+
+def gauge_edit_frame():
+    build_edit_frame()
+
+
+def data_edit_frame():
+    build_edit_frame()
+
+
+def map_edit_frame():
+    build_edit_frame()
+
+
+def control_edit_frame():
+    build_edit_frame()
+
+
+def icon_edit_frame():
+    build_edit_frame()
+
+
+def table_edit_frame():
+    build_edit_frame()
 
 
 build_page = tk.Frame(root, background=background_color)
@@ -215,7 +317,8 @@ edit_button.grid(row=0, column=2, padx=1, pady=1)
 # Widget Frame
 widget_width = 5
 widget_height = 10
-widget_title = tk.Label(widget_frame, text="Widgets", font=("Roboto", 30), background=background_color, foreground="white")
+widget_title = tk.Label(widget_frame, text="Widgets", font=("Roboto", 30), background=background_color,
+                        foreground="white")
 widget_title.grid(row=0, column=0, columnspan=3, sticky="swen")
 
 label_widget = ttk.Button(widget_frame, text="Label", command=lambda: add_widget_to_tree("label"))
